9e4b851317d9331218949baa07249b75f6ce6be1
Philip Rosen <philipjrosen@gmail.com>
2018-03-25 12:16:42 -0700

    create async action for fetching books and update reducer

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -1,9 +1,27 @@
-export const createBook = book => {
-  // Return action
+import Axios from "axios";
+// API URL
+const apiUrl = "http://57c62fdcc1fc8711008f2a7e.mockapi.io/api/book";
+// Sync Action
+export const fetchBooksSuccess = books => {
   return {
-    // Unique identifier
-    type: "CREATE_BOOK",
-    // Payload
-    book: book
+    type: "FETCH_BOOKS_SUCCESS",
+    books
+  };
+};
+//Async Action
+export const fetchBooks = () => {
+  // Returns a dispatcher function
+  // that dispatches an action at a later time
+  return dispatch => {
+    // Returns a promise
+    return Axios.get(apiUrl)
+      .then(response => {
+        // Dispatch another action
+        // to consume data
+        dispatch(fetchBooksSuccess(response.data));
+      })
+      .catch(error => {
+        throw error;
+      });
   };
 };
