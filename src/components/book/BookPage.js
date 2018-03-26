9db1b19ecbc6f4846a02c0aa96883c3a27290557
Philip Rosen <philipjrosen@gmail.com>
2018-03-26 14:01:18 -0700

    add BookForm and refactor BookPage

--- src/components/book/BookPage.js
+++ src/components/book/BookPage.js
@@ -1,67 +1,64 @@
 import React from "react";
 import { connect } from "react-redux";
+import BookForm from "./BookForm";
+import { Link } from "react-router-dom";
 import * as bookActions from "../../actions/bookActions";
 
 class Book extends React.Component {
   constructor(props) {
-    // Pass props back to parent
     super(props);
   }
 
-  // Submit book handler
   submitBook(input) {
     this.props.createBook(input);
   }
 
   render() {
-    // Title input tracker
     let titleInput;
-    // return JSX
     return (
-      <div>
-        <h3>Books</h3>
-        <ul>
-          {/* Traverse books array  */}
-          {this.props.books.map((b, i) => <li key={i}>{b.title}</li>)}
-        </ul>
-        <div>
-          <h3>Books Form</h3>
-          <form
-            onSubmit={e => {
-              // Prevent request
-              e.preventDefault();
-              // Assemble inputs
-              var input = { title: titleInput.value };
-              // Call handler
-              this.submitBook(input);
-              // Reset form
-              e.target.reset();
-            }}
-          >
-            <input type="text" name="title" ref={node => (titleInput = node)} />
-            <input type="submit" />
-          </form>
+      <div className="row">
+        <div className="col-md-6">
+          <h3>Books</h3>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.books.map((b, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{b.title}</td>
+                    <td>
+                      <Link to={`/books/${b.id}`}>View</Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-md-6">
+          <h3>New Book</h3>
+          <BookForm submitBook={this.submitBook.bind(this)} />
         </div>
       </div>
     );
   }
 }
 
-// Maps state from store to props
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = state => {
   return {
-    // You can now say this.props.books
     books: state.books
   };
 };
 
-// Maps actions to props
 const mapDispatchToProps = dispatch => {
   return {
-    // You can now say this.props.createBook
     createBook: book => dispatch(bookActions.createBook(book))
   };
 };
 
-// Use connect to put them together
 export default connect(mapStateToProps, mapDispatchToProps)(Book);
